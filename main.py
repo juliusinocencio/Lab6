b721554d082bfed10c46f1ad46aef561240b1f38
juliusinocencio <juliusinocencio@hotmail.com>
2023-03-08 13:51:40 -0600

    v1.1 Added correct functionality to encode - Julius

--- main.py
+++ main.py
@@ -2,7 +2,13 @@
 def encode(password):
     new = ''
     for i in password:
-        new += str(int(i) + 3)
+        x = int(i) + 3
+
+        if x > 9:
+            y = x % 9
+            new += str(y-1)
+        else:
+            new += str(x)
     return new
 
 def decode():
@@ -11,27 +17,28 @@ def decode():
 
 if __name__ == "__main__":
 
-    stored_password = ''
+    #stored_password = ''
+    menu_open = True
+    
+    while menu_open:
+        print(f'Menu\n'
+              f'-------------\n'
+              f'1. Encode\n'
+              f'2. Decode\n'
+              f'3. Quit\n')
+        option = int(input('Please enter an option: '))
 
-    print(f'Menu\n'
-          f'-------------\n'
-          f'1. Encode\n'
-          f'2. Decode\n'
-          f'3. Quit\n')
-    option = int(input('Please enter an option: '))
-    password = input('Please enter your password to encode: ')
-
-    Continue = True
-
-    while Continue:
         if option == 1:
+            password = input('Please enter your password to encode: ')
             stored_password = encode(password)
             print('Your password has been encoded and stored!')
             Continue = False
+
         elif option == 2:
             print('In progress')
 
         elif option == 3:
-            Continue = False
+            #print(stored_password)  #FOR TESTING PURPOSE
+            menu_open = False
 
 
